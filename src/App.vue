687d622d7ce01aa019e7ade1b2bfa8ea6ab7a350
Denis Nesterov <ddnesterov@mts.ru>
2025-03-07 20:48:37 +0300

    Merge fixes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Square from './components/Square.vue';
+</script>
+
+<template>
+  <section class="container">
+    <Square/>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+}
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="square">
+
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.square {
+    height: 200px;
+    width: 200px;
+    color: black;
+}
+</style>
